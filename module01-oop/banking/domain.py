5f840d4b34d402cd810e280e00bf5b96bed347e3
Binnur KURT <deepcloudlabs@gmail.com>
2022-10-05 13:56:44 +0300

    implement inheritance

--- module01-oop/banking/domain.py
+++ module01-oop/banking/domain.py
@@ -28,39 +28,40 @@ class InsufficientBalanceError(Exception):
 
 class Account:
 
+    # constructor
     def __init__(self, iban, balance=0.0, status=AccountStatus.ACTIVE):
         self._iban = iban
         self._balance = balance
         self._status = status
 
-    @property
+    @property  # getter method
     def balance(self):
         return self._balance
 
-    @property
+    @property  # getter method
     def iban(self):
         return self._iban
 
-    @property
+    @property  # getter method
     def status(self):
         return self._status
 
-    @status.setter
+    @status.setter  # setter method
     def status(self, status):
         print("@status.setter status(self, status)")
         if self._status == AccountStatus.CLOSED:
             raise ValueError("Cannot change status for a CLOSED account.")
         self._status = status
 
-    def deposit(self, amount=5.0):
+    def deposit(self, amount=5.0):  # business method
         if amount <= 0:
             raise ValueError("Amount must be positive.")
-        if self.status != AccountStatus.ACTIVE:
-            raise IllegalAccountStateError("Illegal account status to deposit.", self.status)
-        self.balance = self.balance + amount
-        return self.balance
+        if self._status != AccountStatus.ACTIVE:
+            raise IllegalAccountStateError("Illegal account status to deposit.", self._status)
+        self._balance = self._balance + amount
+        return self._balance
 
-    def withdraw(self, amount=5.0):
+    def withdraw(self, amount=5.0):  # business method
         if amount <= 0:
             raise ValueError("Amount must be positive.")
         if self.status != AccountStatus.ACTIVE:
@@ -68,5 +69,39 @@ class Account:
         if amount > self.balance:
             raise InsufficientBalanceError("Your balance does not cover your expenses.",
                                            amount - self.balance)
-        self.balance = self.balance - amount
+        self._balance = self.balance - amount
         return self.balance
+
+    def __str__(self):
+        return f"Account [iban: {self.iban}, balance: {self.balance}, status: {self.status}]"
+
+
+"""
+Account         -> base   /super/parent class
+CheckingAccount -> derived/sub  /child   class   
+"""
+
+
+class CheckingAccount(Account):
+    def __init__(self, iban, balance, status, overdraft_amount):
+        super().__init__(iban, balance, status)
+        self._overdraft_amount = overdraft_amount
+
+    @property  # read-only property
+    def overdraft_amount(self):
+        return self._overdraft_amount
+
+    def withdraw(self, amount=5.0):  # overriding
+        if amount <= 0:
+            raise ValueError("Amount must be positive.")
+        if self.status != AccountStatus.ACTIVE:
+            raise IllegalAccountStateError("Illegal account status to deposit.", self.status)
+        if amount > self.balance + self.overdraft_amount:
+            raise InsufficientBalanceError("Your balance does not cover your expenses.",
+                                           amount - self.balance - self.overdraft_amount)
+        self._balance = self.balance - amount
+        return self.balance
+
+    def __str__(self):  # overriding
+        return f"CheckingAccount [iban: {self.iban}, balance: {self.balance}, status: {self.status}," \
+               f" overdraft_amount: {self._overdraft_amount}]"
--- module01-oop/exercise01.py
+++ module01-oop/exercise01.py
@@ -4,10 +4,9 @@ try:
     account1 = Account("tr1")
     account2 = Account("tr2", 10000)
     account3 = Account("tr3", 20000, AccountStatus.CLOSED)
-    account3.status = AccountStatus.BLOCKED
     account1.deposit(100.0)
     account1.deposit(1.0)
-    print(account1.balance)
+    print(account1)
 except ValueError as err:
     print(err)
 except InsufficientBalanceError as err:
